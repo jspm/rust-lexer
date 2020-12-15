7468cb5015c6d38882cc25a9679382e92083c81e
Guy Bedford <guybedford@gmail.com>
2020-12-15 13:39:18 -0800

    cargo fmt

--- src/parse_error.rs
+++ src/parse_error.rs
@@ -1,42 +1,42 @@
 #[derive(Debug)]
 pub struct ParseError {
-  pub idx: usize,
-  pub line: usize,
-  pub col: usize,
+    pub idx: usize,
+    pub line: usize,
+    pub col: usize,
 }
 
 impl ParseError {
-	pub fn from_source_and_index(source: &str, idx: usize) -> ParseError {
-		let substring = match source.get(0..idx + 1) {
-			None => {
-				return ParseError {
-					idx,
-					line: 0,
-					col: 0,
-				}
-			}
-			Some(s) => s,
-		};
+    pub fn from_source_and_index(source: &str, idx: usize) -> ParseError {
+        let substring = match source.get(0..idx + 1) {
+            None => {
+                return ParseError {
+                    idx,
+                    line: 0,
+                    col: 0,
+                }
+            }
+            Some(s) => s,
+        };
 
-		let (line, last_line) = substring
-			.lines()
-			.fold((0, None), |(count, _), line| (count + 1, Some(line)));
+        let (line, last_line) = substring
+            .lines()
+            .fold((0, None), |(count, _), line| (count + 1, Some(line)));
 
-		ParseError {
-			idx,
-			line,
-			col: last_line.unwrap().len(),
-		}
-	}
+        ParseError {
+            idx,
+            line,
+            col: last_line.unwrap().len(),
+        }
+    }
 }
 
 #[cfg(test)]
 mod tests {
-	use crate::parse_error::ParseError;
+    use crate::parse_error::ParseError;
 
-	#[test]
-	fn basic() {
-		let source = r#"
+    #[test]
+    fn basic() {
+        let source = r#"
 			import './export.js';
 			import { s as p } from './reexport1.js';
 			//     ^
@@ -44,17 +44,17 @@ mod tests {
 			import d from './export.js';
 		"#;
 
-		let err_idx = source.find('{').unwrap();
-		let err = ParseError::from_source_and_index(source, err_idx);
-		assert_eq!(err.line, 3);
-		assert_eq!(err.col, 11);
-	}
+        let err_idx = source.find('{').unwrap();
+        let err = ParseError::from_source_and_index(source, err_idx);
+        assert_eq!(err.line, 3);
+        assert_eq!(err.col, 11);
+    }
 
-	#[test]
-	fn empty_source() {
-		let source = "";
-		let err = ParseError::from_source_and_index(source, 42);
-		assert_eq!(err.line, 0);
-		assert_eq!(err.col, 0);
-	}
+    #[test]
+    fn empty_source() {
+        let source = "";
+        let err = ParseError::from_source_and_index(source, 42);
+        assert_eq!(err.line, 0);
+        assert_eq!(err.col, 0);
+    }
 }
